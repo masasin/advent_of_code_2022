d571178cc4de8cf2943c8859f1d4e0b998bc459f
Jean Nassar <jean@adimian.com>
2022-12-02 20:56:37 +0100

    Do Day 2.

---
+++ solutions/day_02.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+from typing import Generator
+
+
+import numpy as np
+
+
+SHAPE_SCORES = np.array(
+    [
+        [1, 1, 1],
+        [2, 2, 2],
+        [3, 3, 3],
+    ]
+)
+
+OUTCOMES = np.array(
+    [
+        [0, -1, 1],
+        [1, 0, -1],
+        [-1, 1, 0],
+    ]
+)
+
+OUTCOME_SCORES = (OUTCOMES + 1) * 3
+TOTAL_SCORES = SHAPE_SCORES + OUTCOME_SCORES
+
+REVERSE = np.array(
+    [
+        [2, 0, 1],
+        [0, 1, 2],
+        [1, 2, 0],
+    ]
+)
+
+MOVE = "ABC"
+COMPLEMENT = "XYZ"
+
+
+def pair_score(opponent: str, player: str) -> int:
+    player_index = MOVE.index(player)
+    opponent_index = MOVE.index(opponent)
+    return TOTAL_SCORES[player_index, opponent_index]
+
+
+def parse_part_1(text: str) -> Generator:
+    for line in text.splitlines():
+        opponent, player = line.split()
+        yield pair_score(opponent, MOVE[COMPLEMENT.index(player)])
+
+
+def reverse_solve(opponent: str, outcome: str) -> str:
+    opponent_index = MOVE.index(opponent)
+    outcome_index = COMPLEMENT.index(outcome)
+    return MOVE[REVERSE[opponent_index, outcome_index]]
+
+
+def parse_part_2(text: str) -> Generator:
+    for line in text.splitlines():
+        opponent, outcome = line.split()
+        player = reverse_solve(opponent, outcome)
+        yield pair_score(opponent, player)
+
+
+def main():
+    data_part_1 = list(parse_part_1(Path("../inputs/day_02.txt").read_text()))
+    print(f"Part 1: {sum(data_part_1)}")
+
+    data_part_2 = list(parse_part_2(Path("../inputs/day_02.txt").read_text()))
+    print(f"Part 2: {sum(data_part_2)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day_02.py
@@ -0,0 +1,57 @@
+from textwrap import dedent
+
+import pytest
+
+from solutions.day_02 import pair_score, reverse_solve, parse_part_1, parse_part_2
+
+
+@pytest.fixture
+def text():
+    return dedent(
+        """A Y
+        B X
+        C Z
+        """
+    )
+
+
+@pytest.fixture
+def data_part_1():
+    return [8, 1, 6]
+
+
+@pytest.fixture
+def data_part_2():
+    return [4, 1, 7]
+
+
+@pytest.mark.parametrize(
+    ["opponent", "player", "score"],
+    [
+        ("A", "B", 8),
+        ("B", "A", 1),
+        ("C", "C", 6),
+    ],
+)
+def test_pair_score(opponent, player, score):
+    assert pair_score(opponent, player) == score
+
+
+@pytest.mark.parametrize(
+    ["opponent", "end_state", "selection"],
+    [
+        ("A", "Y", "A"),
+        ("B", "X", "A"),
+        ("C", "Z", "A"),
+    ],
+)
+def test_reverse_solve(opponent, end_state, selection):
+    assert reverse_solve(opponent, end_state) == selection
+
+
+def test_parse_part_1(text, data_part_1):
+    assert list(parse_part_1(text)) == data_part_1
+
+
+def test_parse_part_2(text, data_part_2):
+    assert list(parse_part_2(text)) == data_part_2
