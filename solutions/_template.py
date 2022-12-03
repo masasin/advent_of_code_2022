0f695d154ea6c6c0f8a5f4de4f47f1db6bfa2b8b
Jean Nassar <jean@adimian.com>
2022-12-03 12:42:17 +0100

    Add template.

---
+++ solutions/_template.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Generator, Iterable
+
+
+def parse_part_1(text: str) -> Generator:
+    ...
+
+
+def parse_part_2(text: str) -> Generator:
+    ...
+
+
+def solve_part_1(data: Iterable):
+    ...
+
+
+def solve_part_2(data: Iterable):
+    ...
+
+
+def main():
+    text = Path("../inputs/day_02.txt").read_text()
+    print(f"Part 1: {solve_part_1(parse_part_1(text))}")
+    print(f"Part 2: {solve_part_2(parse_part_2(text))}")
+
+
+if __name__ == "__main__":
+    main()
