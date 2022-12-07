d41af261f03f4929910e2ddac53976a496e60280
Jean Nassar <jean@adimian.com>
2022-12-07 14:38:32 +0100

    WIP: Day 7 Part 1
    All tests pass, but I'm apparently undercounting something.

---
+++ solutions/day_07.py
@@ -0,0 +1,83 @@
+import functools as ft
+import operator as op
+from pathlib import Path
+from typing import Generator, Union
+
+
+Tree = dict[str, Union[int, "Tree"]]
+
+
+def _node(path: Path, tree: Tree) -> Tree | int:
+    return ft.reduce(op.getitem, path.parts, tree)  # noqa
+
+
+def walk_dir(path: Path | str, tree: Tree) -> Generator[tuple[Path, int], None, None]:
+    path = Path(path)
+    value = _node(path, tree)
+    match value:
+        case dict():
+            for key in value.keys():
+                yield from walk_dir(path / key, tree)
+        case int():
+            yield path, value
+
+
+def dir_size(path: Path, tree: Tree) -> int:
+    return sum(size for file, size in walk_dir(path, tree))
+
+
+def get_dirs(tree: Tree) -> set[Path]:
+    return set(file.parent for file, size in walk_dir("/", tree))
+
+
+def dirs_smaller_than(
+    max_size: int, tree: Tree
+) -> Generator[tuple[Path, int], None, None]:
+    for dir_ in get_dirs(tree):
+        if (size := dir_size(dir_, tree)) <= max_size:
+            yield dir_, size
+
+
+def move_dir(target: str, path: Path) -> Path:
+    match target:
+        case "/":
+            return Path("/")
+        case "..":
+            return path.parent
+        case other:
+            return path / other
+
+
+def parse(text: str) -> Tree:
+    tree: Tree = {"/": {}}
+    path = Path("/")
+    for line in text.splitlines():
+        match line.split():
+            case ["$", "cd", target]:
+                path = move_dir(target, path)
+            case ["$", *_]:
+                pass
+            case ["dir", name]:
+                _node(path, tree)[name] = {}
+            case [size, name]:
+                _node(path, tree)[name] = int(size)
+    return tree
+
+
+def solve_part_1(data: Tree) -> int:
+    return sum(size for dir_, size in dirs_smaller_than(100_000, data))
+
+
+def solve_part_2(data: Tree) -> int:
+    ...
+
+
+def main():
+    text = Path("../inputs/day_07.txt").read_text()
+    data = parse(text)
+    print(f"Part 1: {solve_part_1(data)}")
+    print(f"Part 2: {solve_part_2(data)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day_07.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+from textwrap import dedent
+
+import pytest
+
+from solutions.day_07 import (
+    dirs_smaller_than,
+    parse,
+    dir_size,
+    solve_part_1,
+    walk_dir,
+    get_dirs,
+    move_dir,
+)
+
+
+@pytest.fixture
+def text():
+    return dedent(
+        """\
+        $ cd /
+        $ ls
+        dir a
+        14848514 b.txt
+        8504156 c.dat
+        dir d
+        $ cd a
+        $ ls
+        dir e
+        29116 f
+        2557 g
+        62596 h.lst
+        $ cd e
+        $ ls
+        584 i
+        $ cd ..
+        $ cd ..
+        $ cd d
+        $ ls
+        4060174 j
+        8033020 d.log
+        5626152 d.ext
+        7214296 k
+        """
+    )
+
+
+@pytest.fixture
+def data():
+    return {
+        "/": {
+            "a": {
+                "e": {
+                    "i": 584,
+                },
+                "f": 29_116,
+                "g": 2557,
+                "h.lst": 62_596,
+            },
+            "b.txt": 14_848_514,
+            "c.dat": 8_504_156,
+            "d": {
+                "j": 4_060_174,
+                "d.log": 8_033_020,
+                "d.ext": 5_626_152,
+                "k": 7_214_296,
+            },
+        }
+    }
+
+
+def test_walk_dir(data):
+    assert set(walk_dir(Path("/a"), data)) == {
+        (Path("/a/e/i"), 584),
+        (Path("/a/f"), 29_116),
+        (Path("/a/g"), 2557),
+        (Path("/a/h.lst"), 62_596),
+    }
+
+
+def test_get_dirs(data):
+    assert get_dirs(data) == {Path("/"), Path("/a"), Path("/a/e"), Path("/d")}
+
+
+@pytest.mark.parametrize(
+    ["folder", "size"],
+    [
+        (Path("/a/e"), 584),
+        (Path("/a"), 94_853),
+        (Path("/d"), 24_933_642),
+        (Path("/"), 48_381_165),
+    ],
+)
+def test_dir_size(data, folder, size):
+    assert dir_size(folder, data) == size
+
+
+def test_dirs_smaller_than(data, data_part_1):
+    assert set(dirs_smaller_than(100_000, data)) == {
+        (Path("/a/e"), 584),
+        (Path("/a"), 94_853),
+    }
+
+
+@pytest.mark.parametrize(
+    ["target", "path", "expected"],
+    [
+        ("/", Path(), Path("/")),
+        ("a", Path("/"), Path("/a")),
+        ("e", Path("/a"), Path("/a/e")),
+        ("..", Path("/a/e"), Path("/a")),
+        ("..", Path("/"), Path("/")),
+        ("d", Path("/"), Path("/d")),
+    ],
+)
+def test_move_dir(target, path, expected):
+    assert move_dir(target, path) == expected
+
+
+def test_parse(text, data):
+    assert parse(text) == data
+
+
+def test_solve_part_1(data):
+    assert solve_part_1(data) == 95_437
