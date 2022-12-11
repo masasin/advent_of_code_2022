509fe2c2ef4fe1d1dbfdff0eca5e64d80d875c71
Jean Nassar <jean@adimian.com>
2022-12-11 21:44:54 +0100

    Solve Day 10.

---
+++ solutions/day_10.py
@@ -0,0 +1,61 @@
+import itertools as it
+from pathlib import Path
+from typing import Generator, Iterable
+
+
+class Sim:
+    def __init__(self, start: int = 20, stride: int = 40):
+        self.sprite_position: int = 1
+        self.interesting_numbers = it.count(start=start, step=stride)
+        self.pixel_position = 0
+
+    def run_part_1(self, values: Iterable[int]) -> Generator[int, None, None]:
+        next_interesting = next(self.interesting_numbers)
+        for i, value in enumerate(values, start=1):
+            if i == next_interesting:
+                yield i * self.sprite_position
+                next_interesting = next(self.interesting_numbers)
+            self.step(value)
+
+    def run_part_2(self, values: Iterable[int]) -> Generator[str, None, None]:
+        for value in values:
+            line_end = "\n" if self.pixel_position == 39 else ""
+            if abs(self.pixel_position - self.sprite_position) <= 1:
+                yield "#" + line_end
+            else:
+                yield "." + line_end
+            self.step(value)
+
+    def step(self, value):
+        self.sprite_position += value
+        self.pixel_position = (self.pixel_position + 1) % 40
+
+
+def parse(text: str) -> Generator[int, None, None]:
+    for line in text.splitlines():
+        match line.split():
+            case ["noop"]:
+                yield 0
+            case ["addx", number]:
+                yield 0
+                yield int(number)
+
+
+def solve_part_1(data: Iterable[int]) -> int:
+    sim = Sim()
+    return sum(sim.run_part_1(data))
+
+
+def solve_part_2(data: Iterable[int]) -> str:
+    sim = Sim()
+    return "\n" + "".join(sim.run_part_2(data))
+
+
+def main():
+    text = Path("../inputs/day_10.txt").read_text()
+    print(f"Part 1: {solve_part_1(parse(text))}")
+    print(f"Part 2: {solve_part_2(parse(text))}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day_10.py
@@ -0,0 +1,208 @@
+from textwrap import dedent
+
+import pytest
+
+from solutions.day_10 import parse, solve_part_1, Sim, solve_part_2
+
+
+@pytest.fixture
+def text():
+    return dedent(
+        """\
+        addx 15
+        addx -11
+        addx 6
+        addx -3
+        addx 5
+        addx -1
+        addx -8
+        addx 13
+        addx 4
+        noop
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx -35
+        addx 1
+        addx 24
+        addx -19
+        addx 1
+        addx 16
+        addx -11
+        noop
+        noop
+        addx 21
+        addx -15
+        noop
+        noop
+        addx -3
+        addx 9
+        addx 1
+        addx -3
+        addx 8
+        addx 1
+        addx 5
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx -36
+        noop
+        addx 1
+        addx 7
+        noop
+        noop
+        noop
+        addx 2
+        addx 6
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx 1
+        noop
+        noop
+        addx 7
+        addx 1
+        noop
+        addx -13
+        addx 13
+        addx 7
+        noop
+        addx 1
+        addx -33
+        noop
+        noop
+        noop
+        addx 2
+        noop
+        noop
+        noop
+        addx 8
+        noop
+        addx -1
+        addx 2
+        addx 1
+        noop
+        addx 17
+        addx -9
+        addx 1
+        addx 1
+        addx -3
+        addx 11
+        noop
+        noop
+        addx 1
+        noop
+        addx 1
+        noop
+        noop
+        addx -13
+        addx -19
+        addx 1
+        addx 3
+        addx 26
+        addx -30
+        addx 12
+        addx -1
+        addx 3
+        addx 1
+        noop
+        noop
+        noop
+        addx -9
+        addx 18
+        addx 1
+        addx 2
+        noop
+        noop
+        addx 9
+        noop
+        noop
+        noop
+        addx -1
+        addx 2
+        addx -37
+        addx 1
+        addx 3
+        noop
+        addx 15
+        addx -21
+        addx 22
+        addx -6
+        addx 1
+        noop
+        addx 2
+        addx 1
+        noop
+        addx -10
+        noop
+        noop
+        addx 20
+        addx 1
+        addx 2
+        addx 2
+        addx -6
+        addx -11
+        noop
+        noop
+        noop
+        """
+    )
+
+
+@pytest.fixture
+def data(text):
+    return list(parse(text))
+
+
+@pytest.fixture
+def signal_strengths():
+    return [420, 1140, 1800, 2940, 2880, 3960]
+
+
+@pytest.fixture
+def screen():
+    return dedent(
+        """\
+        ##..##..##..##..##..##..##..##..##..##..
+        ###...###...###...###...###...###...###.
+        ####....####....####....####....####....
+        #####.....#####.....#####.....#####.....
+        ######......######......######......####
+        #######.......#######.......#######.....
+        """
+    )
+
+
+def test_parse_part_1(data):
+    text = dedent(
+        """\
+        noop
+        addx 3
+        addx -5
+        """
+    )
+    expected = [0, 0, 3, 0, -5]
+    assert list(parse(text)) == expected
+
+
+def test_run(data, signal_strengths):
+    sim = Sim()
+    assert list(sim.run_part_1(data)) == signal_strengths
+
+
+def test_solve_part_1(data):
+    assert solve_part_1(data) == 13140
+
+
+def test_solve_part_2(data, screen):
+    assert solve_part_2(data)[1:] == screen
