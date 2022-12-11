bfc6be227778e0ddc5a494ff6ff13256b1b575b4
Jean Nassar <jean@adimian.com>
2022-12-11 22:49:51 +0100

    Avoid duplicate parsing.

--- solutions/day_10.py
+++ solutions/day_10.py
@@ -53,8 +53,9 @@ def solve_part_2(data: Iterable[int]) -> str:
 
 def main():
     text = Path("../inputs/day_10.txt").read_text()
-    print(f"Part 1: {solve_part_1(parse(text))}")
-    print(f"Part 2: {solve_part_2(parse(text))}")
+    data = list(parse(text))
+    print(f"Part 1: {solve_part_1(data)}")
+    print(f"Part 2: {solve_part_2(data)}")
 
 
 if __name__ == "__main__":
