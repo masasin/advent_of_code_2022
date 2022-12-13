209804932adf31395ee6253767b3e82f6a8d930c
Jean Nassar <jean@adimian.com>
2022-12-13 18:20:09 +0100

    Solve Day 13 Part 2.

--- solutions/day_13.py
+++ solutions/day_13.py
@@ -1,3 +1,6 @@
+import functools as ft
+import itertools as it
+import math
 from pathlib import Path
 from typing import Generator, Iterable
 
@@ -6,7 +9,10 @@ Packet = list[PacketPart]
 PacketPair = tuple[Packet, Packet]
 
 
-def parse_part_1(text: str) -> Generator[PacketPair, None, None]:
+DECODER_PACKETS = [[[2]], [[6]]]
+
+
+def parse(text: str) -> Generator[PacketPair, None, None]:
     parts = text.split("\n\n")
     for part in parts:
         left, right = map(eval, part.splitlines())
@@ -44,19 +50,23 @@ def solve_part_1(pairs: Iterable[PacketPair]) -> int:
     return sum(i for i, pair in enumerate(pairs, start=1) if is_right_order(*pair))
 
 
-def parse_part_2(text: str) -> Generator:
-    ...
+def reorder_packets(pairs: Iterable[PacketPair]) -> list:
+    return sorted(
+        list(it.chain.from_iterable(pairs)) + DECODER_PACKETS,
+        key=ft.cmp_to_key(lambda left, right: -1 if is_right_order(left, right) else 1),
+    )
 
 
-def solve_part_2(data: Iterable):
-    ...
+def solve_part_2(pairs: Iterable[PacketPair]) -> int:
+    reordered = reorder_packets(pairs)
+    return math.prod(reordered.index(packet) + 1 for packet in DECODER_PACKETS)
 
 
 def main():
     text = Path("../inputs/day_13.txt").read_text()
-    pairs = parse_part_1(text)
+    pairs = list(parse(text))
     print(f"Part 1: {solve_part_1(pairs)}")
-    print(f"Part 2: {solve_part_2(parse_part_2(text))}")
+    print(f"Part 2: {solve_part_2(pairs)}")
 
 
 if __name__ == "__main__":
--- tests/test_day_13.py
+++ tests/test_day_13.py
@@ -2,7 +2,13 @@ from textwrap import dedent
 
 import pytest
 
-from solutions.day_13 import parse_part_1, is_right_order, solve_part_1
+from solutions.day_13 import (
+    parse,
+    is_right_order,
+    solve_part_1,
+    reorder_packets,
+    solve_part_2,
+)
 
 
 @pytest.fixture
@@ -53,8 +59,32 @@ def data():
     ]
 
 
-def test_parse_part_1(text, data):
-    assert list(parse_part_1(text)) == data
+@pytest.fixture
+def reordered():
+    return [
+        [],
+        [[]],
+        [[[]]],
+        [1, 1, 3, 1, 1],
+        [1, 1, 5, 1, 1],
+        [[1], [2, 3, 4]],
+        [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+        [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+        [[1], 4],
+        [[2]],
+        [3],
+        [[4, 4], 4, 4],
+        [[4, 4], 4, 4, 4],
+        [[6]],
+        [7, 7, 7],
+        [7, 7, 7, 7],
+        [[8, 7, 6]],
+        [9],
+    ]
+
+
+def test_parse(text, data):
+    assert list(parse(text)) == data
 
 
 @pytest.mark.parametrize(
@@ -108,3 +138,11 @@ def test_right_order(left, right, expected):
 
 def test_solve_part_1(data):
     assert solve_part_1(data) == 13
+
+
+def test_reorder_packets(data, reordered):
+    assert reorder_packets(data) == reordered
+
+
+def test_solve_part_2(data):
+    assert solve_part_2(data) == 140
